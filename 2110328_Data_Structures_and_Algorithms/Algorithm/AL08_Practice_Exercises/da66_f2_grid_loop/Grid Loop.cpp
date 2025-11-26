9279ab0ee892e69866b48aebfdb6b7127a66e844
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2025-11-26 14:50:22 +0700

    Revise Grid Loop

--- 2110328_Data_Structures_and_Algorithms/Algorithm/AL08_Practice_Exercises/da66_f2_grid_loop/Grid Loop.cpp
+++ 2110328_Data_Structures_and_Algorithms/Algorithm/AL08_Practice_Exercises/da66_f2_grid_loop/Grid Loop.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <tuple>
 using namespace std;
+
+#define Walk tuple<int, int, int>
+#define check(type, d, r, c) (dir[type][d] && dir[V[r][c]][d ^ 1])
 
 int main()
 {
@@ -30,7 +34,48 @@ int main()
     dir[5] = {1, 0, 0, 1};
     dir[6] = {0, 1, 0, 1};
 
-    return 0;
+    queue<Walk> Q;
+    vector<vector<int>> dis(n, vector<int>(m));
+
+    int Mx = 0, cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (!dis[i][j])
+                Q.push({i, j, 1});
+            while (Q.size())
+            {
+                auto [r, c, w] = Q.front();
+                Q.pop();
+
+                if (dis[r][c] && dis[r][c] < w)
+                    continue;
+
+                if (dis[r][c] == w)
+                {
+                    cnt++;
+                    int cycle_size = dis[r][c] * 2 - 2;
+                    Mx = max(Mx, cycle_size);
+                    break;
+                }
+
+                dis[r][c] = w;
+                int type = V[r][c];
+
+                if (r + 1 < n && check(type, 0, r + 1, c))
+                    Q.push({r + 1, c, w + 1});
+                if (r - 1 >= 0 && check(type, 1, r - 1, c))
+                    Q.push({r - 1, c, w + 1});
+                if (c + 1 < m && check(type, 2, r, c + 1))
+                    Q.push({r, c + 1, w + 1});
+                if (c - 1 >= 0 && check(type, 3, r, c - 1))
+                    Q.push({r, c - 1, w + 1});
+            }
+        }
+    }
+
+    cout << cnt << " " << Mx << "\n";
 
     return 0;
 }
